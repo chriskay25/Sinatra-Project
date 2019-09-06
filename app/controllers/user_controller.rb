b87e57072a093407fe349239981b264886832030
chriskay <chriskay25@gmail.com>
2019-09-06 10:18:57 -0400

    Added views, set up sessions.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,16 @@
 class UserController < ApplicationController
 
     get '/login' do
+        erb :'/users/login'
+    end 
 
+    post '/login' do 
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
     end 
 
     get '/signup' do
-
+        erb :'users/signup'
     end 
 
 end 
